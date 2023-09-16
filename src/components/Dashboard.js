d40e4a356d3f616dd8f920c50e24f286c105de2d
LRey21 <mg.reyastinza@gmail.com>
2023-09-16 16:15:52 -0600

    Library done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
 import Calendar from "react-calendar";
 
-import Spacer from "./Spacer";
-import { Button } from "bootstrap";
 import { CaretLeftFill, CaretRightFill } from "react-bootstrap-icons";
+import MyLibrary from "./MyLibrary";
+import { my_books } from "./data/my_books";
 
 dayjs.locale('en');
 
 const Dashboard = () => {
       
     const todayDate = dayjs().format('dddd, MMMM D');
+    const booksData = my_books;
+    const [readingBooks, setReadingBooks] = useState([]);
+    const [currentBookIndex, setCurrentBookIndex] = useState(0);
+
+    useEffect(() =>{
+        const reading = booksData.filter(book => book.status === 'reading');
+        setReadingBooks(reading);
+    }, [booksData]);
+
+    const handlePrevBook = () => {
+        setCurrentBookIndex(prevIndex => (prevIndex - 1 + readingBooks.length) % readingBooks.length);
+    };
+
+    const handleNextBook = () => {
+        setCurrentBookIndex(prevIndex => (prevIndex + 1) % readingBooks.length);
+      };
 
     return (
         <div className="container-fluid">
@@ -19,14 +35,14 @@ const Dashboard = () => {
             <div className="card">
                 <div className="row container-fluid d-flex align-items-center">
                     <div className="col-sm-1">
-                        <CaretLeftFill type="button" style={{ color: '#DED6D2' }}/>
+                        <CaretLeftFill type="button" style={{ color: '#DED6D2' }} onClick={handlePrevBook}/>
                     </div>
                     <div className="col-sm-10">
                         <p style={{ marginBottom: 0 }}>Reading: </p>
-                        <h1 style={{ marginTop: 0 }}>La Vida en el Arte</h1>
+                        <h1 style={{ marginTop: 0 }}>{readingBooks[currentBookIndex]?.title || "Come on! Read something!"}</h1>
                     </div>
                     <div className="col-sm-1">
-                        <CaretRightFill type="button" style={{ color: '#DED6D2', marginRight: '20px' }}/>
+                        <CaretRightFill type="button" style={{ color: '#DED6D2', marginRight: '20px' }} onClick={handleNextBook}/>
                     </div>
                 </div>
                 <div className="row mt-4">
@@ -34,11 +50,11 @@ const Dashboard = () => {
                     <div className="col-sm-2">
                         <img 
                             className="img-fluid img-sm shadow-sm" 
-                            src="https://aliaseditorial.com/wp-content/uploads/2020/10/portada-clara-porset.jpg" 
-                            alt="La Vida en el Arte"/>
+                            src={readingBooks[currentBookIndex]?.coverPage} 
+                            alt={readingBooks[currentBookIndex]?.title}/>
                     </div>
                     <div className="col-sm-5">
-                        <p>Page <b className="text-bigger">138</b>/332 </p>
+                        <p>Page <b className="text-bigger">{readingBooks[currentBookIndex]?.pagesRead}</b>/{readingBooks[currentBookIndex]?.pages} </p>
                         <div className="mt-4">
                             <p style={{ marginBottom: 3 }}>Today I read</p>
                             <div className="row align-items-center">
@@ -62,11 +78,20 @@ const Dashboard = () => {
                                 <textarea className="form-control" rows="2" id="comment"/>
                             </div>
                         </div>
+                        <div className="d-flex justify-content-end mt-2">
+                          <button className="ok-btn">Ok</button>
+                        </div>
+                        
                     </div>
                     <div className="col-sm-3">
                         <Calendar/>
                     </div>
                 </div>
+            </div>
+            <div className="spacer"></div>
+            <div className="spacer"></div>
+            <div className="container">
+                <MyLibrary booksData={booksData}/>
             </div>
         </div>
     )
--- src/components/MyLibrary.js
+++ src/components/MyLibrary.js
@@ -1,10 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { List, Grid } from "react-bootstrap-icons";
 
-const MyLibrary = () => {
+import { my_books } from "./data/my_books";
+
+const MyLibrary = ({ booksData }) => {
+    const [viewType, setViewType] = useState('list'); 
+    const [booksToRead, setBooksToRead] = useState([]);
+    const [booksReading, setBooksReading] = useState([]);
+    const [booksRead, setBooksRead] = useState([]);
+
+    useEffect(() => {
+        const toRead = booksData.filter(book => book.status === 'to read');
+        const reading = booksData.filter(book => book.status == 'reading');
+        const read = booksData.filter(book => book.status == "read");
+
+        setBooksToRead(toRead);
+        setBooksReading(reading);
+        setBooksRead(read);
+    }, [booksData]);
+  
+    const toggleView = (type) => {
+        setViewType(type);
+      };
+    
+    const renderBookList = (books) => {
+      return books.map((book, index) => (
+        <li key={index}>
+          {viewType === 'gallery' ? (
+            <img className="book-cover column-sm" src={book.coverPage} alt={book.title} />
+          ) : (
+            book.title
+          )}
+        </li>
+      ));
+    };
+
     return (
-        <div>
-            <h1>My Library</h1>
-        </div>
-    )
-};
+        <div className="container-fluid mt-4">
+          <div className="row">
+            <h3 className="text-center">My Library</h3>
+          </div>
+          <div className="row">
+            <div className="btn-toolbar mb-3 justify-content-end">
+                <div className="btn-group">
+                    <button
+                        className={`view-btn ${viewType === 'list' ? 'btn-primary' : 'btn-secondary'}`}
+                        onClick={() => toggleView('list')}
+                        style={{ marginRight: '10px'}}
+                    >
+                        <List className="icon"/>
+                    </button>
+                    <button
+                        className={`view-btn ${viewType === 'gallery' ? 'btn-primary' : 'btn-secondary'}`}
+                        onClick={() => toggleView('gallery')}
+                    >
+                        <Grid className="icon"/>
+                    </button>
+                </div>
+            </div>
+          </div>
+          <div className="spacer-sm"></div>
+          <div className="row container-fluid justify-content-center">
+          <div className="col list-reading">
+            <h5 className="mb-5 text-center">To Read</h5>
+            <div className={`d-flex ${viewType === 'gallery' ? 'flex-wrap' : ''}`}>
+              <ul className={viewType === 'gallery' ? 'gallery-list' : ''}>
+                    {renderBookList(booksToRead)}
+                </ul>
+            </div>
+          </div>
+          <div className="col list-reading">
+            <h5 className="mb-5 text-center">Reading</h5>
+            <div className={`d-flex ${viewType === 'gallery' ? 'flex-wrap' : ''}`}>
+              <ul className={viewType === 'gallery' ? 'gallery-list' : ''}>
+                {renderBookList(booksReading)}
+              </ul>
+            </div>
+          </div>
+          <div className="col">
+            <h5 className="mb-5 text-center">Read</h5>
+              <div className={`d-flex ${viewType === 'gallery' ? 'flex-wrap' : ''}`}>
+                <ul className={viewType === 'gallery' ? 'gallery-list' : ''}>
+                  {renderBookList(booksRead)}
+                </ul>
+              </div>
+            </div>
+          </div>
+        <div className="spacer"></div>
+      </div>
+    );
+  };
+  
 export default MyLibrary
