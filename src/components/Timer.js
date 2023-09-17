746abb35b04fa0260d4710d71a028f23671154c5
LRey21 <mg.reyastinza@gmail.com>
2023-09-17 17:01:10 -0600

    starting timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Timer = () => {
+
+    const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false)
+
+    useEffect(() => {
+        let interval;
+        if (running) {
+          interval = setInterval(() => {
+            setTime((prevTime) => prevTime + 10);
+          }, 10);
+        } else if (!running) {
+          clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+      }, [running]);
+
     return (
-        <div>
-            <h1>Timer</h1>
+        <div className="container-fluid ">
+            <div className="spacer"></div>
+            <h1 className="mt-6 text-center">Timer</h1>
+            <div className="spacer-sm"></div>
+            <div className="stopwatch">
+                <div className="align-self-center numbers">
+                    <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+                    <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+                    <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+                </div>
+                <div className="mt-6">
+                    <button className="btn-timer" onClick={() => setRunning(true)}>Start</button>
+                    <button className="btn-timer" onClick={() => setRunning(false)}>Stop</button>
+                    <button className="btn-timer" onClick={() => setTime(0)}>Reset</button>       
+                </div>
+            </div>
+          
         </div>
     )
 };
